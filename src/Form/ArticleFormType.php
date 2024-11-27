39ba04134574ec5ee57b7b2d9e0cdab350fc3ad4
binyoma <innocentbinyoma@pm.me>
2024-11-28 00:09:22 +0100

    Form Type class & EntityType

---
+++ src/Form/ArticleFormType.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ArticleFormType extends AbstractType
+{
+    private UserRepository $userRepository;
+
+    public function __construct(UserRepository $userRepository)
+    {
+        $this->userRepository = $userRepository;
+    }
+
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+       $builder
+           ->add('title', TextType::class, [
+               'help' => 'Choose something catchy!',
+           ])
+           ->add('content')
+           ->add('publishedAt', null, [
+               'widget' => 'single_text',
+           ])
+           ->add('author', EntityType::class, [
+               'class'=> User::class,
+               'choice_label' => function(User $user) {
+               return sprintf('(%d) %s', $user->getId(), $user->getEmail());
+               },
+               'placeholder' => 'Choose author',
+               'choices' => $this->userRepository->findAllEmailAlphabetical(),
+           ])
+       ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+       $resolver->setDefaults([
+           'data_class' => 'App\Entity\Article'
+       ]);
+    }
+
+
+}
