7169487aead824844d9bb722a489c98ccd7b4f9f
binyoma <innocentbinyoma@pm.me>
2024-12-19 10:20:54 +0100

    dependent select fields

--- src/Controller/ArticleAdminController.php
+++ src/Controller/ArticleAdminController.php
@@ -12,7 +12,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class ArticleAdminController extends AbstractController
+class ArticleAdminController extends BaseController
 {
     /**
      * @Route("/admin/article/new", name="admin_article_new")
@@ -61,6 +61,28 @@ public function edit(Article $article, Request $request, EntityManagerInterface 
         return $this->render('article_admin/edit.html.twig', [
             'articleForm' => $form->createView(),
         ]);
+    }
+
+    /**
+     * @Route("/admin/article/location-select",name="admin_article_location_select")
+     * @IsGranted("ROLE_USER")
+     */
+    public function getSpecificLocationSelect(Request $request)
+    {
+        if (!$this->isGranted("ROLE_ADMIN_ARTICLE") && $this->getUser()->getArticles()->isEmpty()){
+            throw $this->createAccessDeniedException();
+        }
+        $article = new Article();
+        $article->setLocation($request->query->get('location'));
+        $form = $this->createForm(ArticleFormType::class, $article);
+        // no field? Return an empty response
+        if(! $form->has('specificLocationName')){
+            return new Response(null, 204);
+        }
+        return $this->render('article_admin/_specific_location.html.twig', [
+            'articleForm' => $form->createView(),
+        ]);
+
     }
 
     /**
--- src/Form/ArticleFormType.php
+++ src/Form/ArticleFormType.php
@@ -2,13 +2,19 @@
 
 namespace App\Form;
 
+use App\Entity\Article;
 use App\Entity\User;
 use App\Repository\UserRepository;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
+use Symfony\Component\Form\FormInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use function Sodium\add;
 
 class ArticleFormType extends AbstractType
 {
@@ -21,6 +27,9 @@ class ArticleFormType extends AbstractType
 
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
+        /**
+         * @var Article|null $article
+         */
        $article = $options['data']?? null;
        $isEdit = $article && $article->getId() ;
        $builder
@@ -34,12 +43,45 @@ class ArticleFormType extends AbstractType
            ->add('author', UserSelectTextType::class, [
                'disabled' => $isEdit,
            ])
-       ;
+           ->add('location', ChoiceType::class, [
+               'placeholder' => 'Choose a location',
+               'choices'=> [
+                   'The Solar System' => 'solar_system',
+                   'Near a star'=> 'star',
+                   'Interstellar  Space'=> 'interstellar_space',
+               ],
+               'required' => false,
+       ]);
+
        if ($options['included_published_at']){
            $builder->add('publishedAt', null, [
                'widget' => 'single_text',
            ]);
        }
+       $builder->addEventListener(
+           FormEvents::PRE_SET_DATA,
+           function (FormEvent $event) {
+               /** @var Article|null $data */
+               $data = $event->getData();
+               if(!$data){
+                   return;
+               }
+               $this->setupSpecificLocationNameField(
+                   $event->getForm(),
+                   $data->getLocation()
+               );
+
+       });
+       $builder->get('location')->addEventListener(
+           FormEvents::POST_SUBMIT,
+           function (FormEvent $event) {
+               $form = $event->getForm();
+               $this->setupSpecificLocationNameField(
+                   $form->getParent(),
+                   $form->getData()
+               );
+           }
+       );
     }
 
     public function configureOptions(OptionsResolver $resolver)
@@ -48,6 +90,56 @@ class ArticleFormType extends AbstractType
            'data_class' => 'App\Entity\Article',
            'included_published_at'=> false,
        ]);
+    }
+    private function getLocationNameChoices(string $location)
+    {
+        $planets = [
+            'Mercury',
+            'Venus',
+            'Earth',
+            'Mars',
+            'Jupiter',
+            'Saturn',
+            'Uranus',
+            'Neptune',
+        ];
+
+        $stars = [
+            'Polaris',
+            'Sirius',
+            'Alpha Centauari A',
+            'Alpha Centauari B',
+            'Betelgeuse',
+            'Rigel',
+            'Other'
+        ];
+
+        $locationNameChoices = [
+            'solar_system' => array_combine($planets, $planets),
+            'star' => array_combine($stars, $stars),
+            'interstellar_space' => null,
+        ];
+
+        return $locationNameChoices[$location]?? null;
+    }
+    private function setupSpecificLocationNameField(FormInterface $form, ?String $location)
+    {
+        if (null === $location){
+            $form->remove('specificLocationName');
+            return ;
+        }
+        $choices = $this->getLocationNameChoices($location);
+        if(null === $choices){
+            $form->remove('specificLocationName');
+            return ;
+        }
+        $form->add('specificLocationName', ChoiceType::class, [
+            'placeholder' => 'Where exactly',
+            'choices'=> $choices,
+            'required' => false,
+        ]);
+
+
     }
 
 
